30630edd233098de92a9406372444db4ffb0b723
killi1812 <cvok.fran@gmail.com>
2024-12-27 22:43:52 +0100

    moved existing conf to .old files

--- link.sh
+++ link.sh
@@ -1,4 +1,10 @@
 !#/bin/zsh
+
+mv ~/.zshrc ~/.zshrc.old
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+mv ~/.gitconfig* ~/.gitconfig*.old
 ln -s ~/dotfiles/.gitconfig* ~/
+
+mv ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf.old
 ln -s ~/dotfiles/neofetch.conf ~/.config/neofetch/config.conf
