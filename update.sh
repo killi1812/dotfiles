cff75b8759d8af30f0cdfbaad92770a64e64d63b
killi1812 <cvok.fran@gmail.com>
2025-01-03 14:47:18 +0100

    Update.sh updated

--- update.sh
+++ update.sh
@@ -11,3 +11,7 @@ ln -sf ~/$dotfiles/neofetch.conf ~/.config/neofetch/config.conf
 ln -sf ~/$dotfiles/ghostty.conf ~/.config/ghostty/config
 
 ln -sf ~/$dotfiles/nvim ~/.config/nvim
+
+ln -s ~/$dotfiles/keyboard/default.conf ~/.config/hypr/conf/keybindings/default.conf
+
+ln -s ~/$dotfiles/keyboard/keyboard.conf ~/.config/hypr/conf/keyboard.conf
