ae474b2391901498b3bcb78c868e40d0ee7681fa
killi1812 <cvok.fran@gmail.com>
2025-01-03 10:33:24 +0100

    Update.sh and link.sh updates

--- link.sh
+++ link.sh
@@ -1,16 +1,18 @@
-!#/bin/zsh
+#!/bin/zsh
+
+dotfiles='dotfiles.personal'
 
 mv ~/.zshrc ~/.zshrc.old
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/$dotfiles/.zshrc ~/.zshrc
 
 mv ~/.gitconfig* ~/.gitconfig*.old
-ln -s ~/dotfiles/.gitconfig* ~/
+ln -s ~/$dotfiles/.gitconfig* ~/
 
 mv ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf.old
-ln -s ~/dotfiles/neofetch.conf ~/.config/neofetch/config.conf
+ln -s ~/$dotfiles/neofetch.conf ~/.config/neofetch/config.conf
 
 mv ~/.config/ghostty/config ~/.config/ghostty/ghostty.old
-ln -s ~/dotfiles/ghostty.conf ~/.config/ghostty/config
+ln -s ~/$dotfiles/ghostty.conf ~/.config/ghostty/config
 
 mv ~/.config/nvim ~/.config/nvim.old
-ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/$dotfiles/nvim ~/.config/nvim
